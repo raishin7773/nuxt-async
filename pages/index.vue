85c59667f9f2bb729c3ecdc51126f403b5f3178b
raishin7773 <raishin7773@yahoo.co.jp>
2021-04-02 16:16:52 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--    {{ users[0].id }},{{ users[0].name }}-->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }},{{ user.name }},{{ user.company.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersx'
+export default {
+  asyncData({params,error}) {
+    return axios.get(url).then((res) => {
+      return {users: res.data}
+    }).catch(e => {
+      error({statusCode: e.response.status, message: 'Error!!!!!'})
+    })
+  }
+}
+</script>
+
+<style>
+
+
+</style>
