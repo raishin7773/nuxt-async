467d9919029612cc80804112a8b17648e02ea7db
raishin7773 <raishin7773@yahoo.co.jp>
2021-04-10 09:54:02 +0900

    actionの利用 モジュールモードへ変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <p>{{ $store.state.message }}</p>
+<!--    <p>{{ $store.state.message }}</p>-->
+    <p>{{ $store.state.hello.message }}</p>
 <!--    <button v-on:click="$store.commit('updateMessage')">更新ボタン</button>-->
-    <button v-on:click="$store.commit('updateMessage','Commit with payload')">更新ボタン</button>
+<!--    <button v-on:click="$store.commit('updateMessage','Commit with payload')">更新ボタン</button>-->
+<!--    <button v-on:click="$store.dispatch('updateMessageAction')">dispatch</button>-->
+<!--    <button v-on:click="$store.dispatch('updateMessageAction','dispatch with payload')">dispatch</button>-->
+    <button v-on:click="$store.dispatch('hello/updateMessageAction','dispatch with payload module')">dispatch</button>
     <img src="~/assets/cat.jpg">
     <ul>
       <li v-for="user in users" :key="user.id">
